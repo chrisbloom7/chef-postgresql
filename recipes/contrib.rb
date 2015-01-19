f9f214a224e49ee469baf80af9fe4807c21501e1
Chris Bloom <chrisbloom7@gmail.com>
2015-01-19 17:33:31 -0500

    Updating extension code to use Postgresql 8.4 syntax

--- recipes/contrib.rb
+++ recipes/contrib.rb
@@ -20,9 +20,7 @@ include_recipe "postgresql::server"
 # Install the PostgreSQL contrib package(s) from the distribution,
 # as specified by the node attributes.
 node['postgresql']['contrib']['packages'].each do |pg_pack|
-
   package pg_pack
-
 end
 
 # Install PostgreSQL contrib extentions into the template1 database,
@@ -32,7 +30,7 @@ if (node['postgresql']['contrib'].attribute?('extensions'))
     bash "install-#{pg_ext}-extension" do
       user 'postgres'
       code <<-EOH
-        echo 'CREATE EXTENSION IF NOT EXISTS "#{pg_ext}";' | psql -d template1
+        psql -d template0 -f `pg_config --sharedir`/contrib/#{pg_ext}.sql
       EOH
       action :run
       ::Chef::Resource.send(:include, Opscode::PostgresqlHelpers)
